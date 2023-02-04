a4effb934f23b5e09c6b405bdc01eded97abce3f
Miott0 <88847519+Miott0@users.noreply.github.com>
2023-02-04 12:09:20 -0300

    Add files via upload

---
+++ imobiliaria/src/main/java/com/imobiliaria/imobiliaria/entities/Corretor.java
@@ -0,0 +1,40 @@
+package com.imobiliaria.imobiliaria.entities;
+
+public class Corretor extends Usuario{
+    private String senha;
+    private boolean aprovado = false;
+
+    public Corretor(){
+
+    }
+
+    public Corretor(String nome, String sobrenome, String email){
+        setNome(nome);
+        setSobrenome(sobrenome);
+        setEmail(email);
+    }
+    
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean getAprovado(){
+        return aprovado;
+    }
+    
+    public void CadastrarCorretor(){
+
+    }
+
+    public boolean AprovarCorretor(){
+        if (aprovado == false) {
+            return aprovado = true;
+        }
+        return true; 
+    }
+
+}
